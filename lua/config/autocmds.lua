7680c0d18c65c3644aa9c9dd5d6b910fc35efb44
Miikka Valkonen <miikka@valkonen.cc>
2025-05-10 14:05:38 +0300

    initial commit

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,16 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("Filetype", {
+  pattern = "norg",
+  callback = function()
+    vim.keymap.set("n", "<C-Space>", "Plug>(neorg.qol.todo-items.todo.task-cycle)", { buffer = true })
+    vim.keymap.set("n", "<CR>", "Plug>(neorg.esupports.hop.hop-link)", { buffer = true })
+  end,
+})
