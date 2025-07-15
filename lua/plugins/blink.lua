fd850024d3ba7d04a75d50db9061d089376350a8
Miikka Valkonen <miikka.valkonen@if.fi>
2025-07-15 13:52:54 +0000

    blink.cmp: added easy-dotnet provider

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,5 +7,19 @@ return {
       preset = "super-tab",
       ["<ENTER>"] = { "select_and_accept" },
     },
+    opts = {
+      sources = {
+        default = { "easy-dotnet" },
+        providers = {
+          ["easy-dotnet"] = {
+            name = "easy-dotnet",
+            enabled = true,
+            module = "easy-dotnet.completion.blink",
+            score_offset = 10000,
+            async = true,
+          },
+        },
+      },
+    },
   },
 }
